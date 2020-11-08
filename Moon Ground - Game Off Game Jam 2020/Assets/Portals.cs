2fcd07b6663642fe36a129ea7cf78a4a6c4567dc
MonteBoomBox <manassjoshi@gmail.com>
2020-11-08 15:44:23 +0530

    Added Portal Mechanic
    Player can now teleport from one place to another using the color-coded Portals.

--- Moon Ground - Game Off Game Jam 2020/Assets/Portals.cs
+++ Moon Ground - Game Off Game Jam 2020/Assets/Portals.cs
@@ -4,35 +4,82 @@ using UnityEngine;
 
 public class Portals : MonoBehaviour
 {
-    Transform destination;
-
-    public bool isBlue;
-    public bool isRed;
+    Vector2 destination;
 
     public float distanceToPortal = 0.3f;
 
-    // Start is called before the first frame update
-    void Start()
+    public GameObject Player;
+
+    string OutName = "Out";
+    string InName = "In";
+
+    private SpriteRenderer PlayerRenderer;
+
+    private float visibleAlphaValue = 1f;
+    private float invisibleAlphaValue = 0f;
+
+
+    public void Start()
     {
-        if (isRed == true)
-        {
-            destination = GameObject.FindGameObjectWithTag("ExitRedPort").GetComponent<Transform>();
-        }
 
-        else if (isBlue == true)
-        {
-            destination = GameObject.FindGameObjectWithTag("ExitBluePort").GetComponent<Transform>();
-        }
     }
-
 
     void OnTriggerEnter2D(Collider2D HitInfo)
     {
-        if (Vector2.Distance(transform.position, HitInfo.transform.position) > distanceToPortal)
+        if (HitInfo.gameObject.tag == "Player")
         {
-            HitInfo.transform.position = new Vector2(destination.position.x, destination.position.y);
+            CheckPortalTag();
+            TeleportPlayer();            
         }
 
+        
 
+    }
+
+    public void CheckPortalTag()
+    {
+        var PortalTag = gameObject.tag;
+
+        if (PortalTag.Contains(InName))
+        {
+            var changedPortalTag = PortalTag.Replace("In", "Out");
+            GameObject PortalToTeleportTo = GameObject.FindGameObjectWithTag(changedPortalTag);
+            destination = PortalToTeleportTo.transform.position;
+        }
+
+        else if (PortalTag.Contains(OutName))
+        {
+            var changedPortalTag = PortalTag.Replace("Out", "In");
+            GameObject PortalToTeleportTo = GameObject.FindGameObjectWithTag(changedPortalTag);
+            destination = PortalToTeleportTo.transform.position;
+        }
+    }
+
+    public void TeleportPlayer()
+    {
+        if (Vector2.Distance(transform.position, Player.transform.position) > distanceToPortal)
+        {
+            FadeOutPlayer();
+            Player.transform.position = new Vector2(destination.x, destination.y);
+            FadeInPlayer();
+        }
+    }
+
+    public void FadeOutPlayer()
+    {
+        SpriteRenderer PlayerAlpha = Player.GetComponent<SpriteRenderer>();
+
+        Color newColor = PlayerAlpha.color;
+        newColor.a = invisibleAlphaValue;
+        PlayerAlpha.color = newColor;
+    }
+
+    public void FadeInPlayer()
+    {
+        SpriteRenderer PlayerAlpha = Player.GetComponent<SpriteRenderer>();
+
+        Color newColor = PlayerAlpha.color;
+        newColor.a = visibleAlphaValue;
+        PlayerAlpha.color = newColor;
     }
 }
---
+++ Moon Ground - Game Off Game Jam 2020/Assets/Scripts/Portal.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    Vector2 PlayerDestination; // The Portal from which the player will come out from
+
+    public GameObject Player; // The player 
+
+    public void OnTriggerEnter2D(Collider2D PortalDetect)
+    {
+        if (PortalDetect.gameObject.CompareTag("Player"))
+        {
+            CheckPortalTag();
+            TeleportPlayer();
+        }
+    }
+
+    public void CheckPortalTag()
+    {
+        var PortalTag = gameObject.tag;
+
+        if (PortalTag.Contains("Start"))
+        {
+            var DestinationPortal = PortalTag + "Exit";
+            GameObject PortalToTeleportTo = GameObject.FindGameObjectWithTag(DestinationPortal);
+            PlayerDestination = PortalToTeleportTo.transform.position;
+        }
+
+        else if (PortalTag.Contains(PortalTag + "Exit"))
+        {
+            var FirstDestinationPortal = PortalTag.Replace("ExitExit", null);   
+            GameObject PortalToTeleportTo = GameObject.FindGameObjectWithTag(FirstDestinationPortal);
+            PlayerDestination = PortalToTeleportTo.transform.position;
+        }        
+    }
+
+    public void TeleportPlayer()
+    {
+        Player.transform.position = PlayerDestination;
+    }
+}
